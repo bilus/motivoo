a6c88eb979967778c4748d3d19f63bc95d0923a7
Martin Bilski <martinb@criticue.com>
2013-05-09 11:11:59 +0200

    I added a hack the middleware to ignore requests for /favicon.ico because when Chrome (and maybe other browsers) do that request, it doesn't pass cookies. This results in a new user being created for every request for a page.

--- lib/rack/motivoo.rb
+++ lib/rack/motivoo.rb
@@ -14,6 +14,8 @@ module Rack
     end
     
     def call(env)
+      return @app.call if env["PATH_INFO"] =~ %r{^/favicon.ico$} # TODO: Make it configurable somehow(?). FIX: No cookies passed when requesting favicon.ico which results in new user data record created for every request.
+        
       ::Motivoo::Context.create(env) do |tracker, request|
         ::Motivoo::Visit.track(tracker, request) do |tracker, request|
           status, headers, body = @app.call(request.env)
