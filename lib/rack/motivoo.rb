5d10faf3cc690305bad9dbd393d2d1a7cae34a37
Martin Bilski <martinb@criticue.com>
2013-05-10 22:23:08 +0200

    FIX: problem with code that was supposed to filter away jpegs, pngs etc. but foremost -- favicons.

--- lib/rack/motivoo.rb
+++ lib/rack/motivoo.rb
@@ -16,7 +16,7 @@ module Rack
     def call(env)
       # FIX: No cookies passed by Chrome when it requests favicon.ico/png which results in new user data record being created for every request. AFAIK, IE keeps a separate cookie store for favicon requests. 
       # Actually, we don't care about binary files at all, so while we're at it, let's ignore them all. In production environment, most of these assets will be fetched from an asset server but it won't hurt to ignore them anyway while at the same time taking care of favicons -- THE serious offenders.
-      return @app.call if env["PATH_INFO"] =~ /\.{jpg|jpeg|png|ico|gif|pdf|tif|tiff|doc|xls|docx|xlsx|css|js}$/i
+      return @app.call(env) if env["PATH_INFO"] =~ /\.jpg|jpeg|png|ico|gif|pdf|tif|tiff|doc|xls|docx|xlsx|css|js$/i
         
       ::Motivoo::Context.create(env) do |tracker, request|
         ::Motivoo::Visit.track(tracker, request) do |tracker, request|
