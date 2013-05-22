3114553d10bfb0e7387d70d7eddfa27092b42901
Martin Bilski <martinb@criticue.com>
2013-05-22 13:40:46 +0200

    Refactored Rack::Motivoo to make the code more readable but, most of all, to also add tracker to env for paths that are skipped because they're binary files (e.g. *.jpg). Wrapped application may want to use tracker for these files as well. The only thing that isn't tracked for binary files is 'visit' and 'first_visit'.

--- lib/rack/motivoo.rb
+++ lib/rack/motivoo.rb
@@ -14,18 +14,32 @@ module Rack
     end
     
     def call(env)
-      # FIX: No cookies passed by Chrome when it requests favicon.ico/png which results in new user data record being created for every request. AFAIK, IE keeps a separate cookie store for favicon requests. 
-      # Actually, we don't care about binary files at all, so while we're at it, let's ignore them all. In production environment, most of these assets will be fetched from an asset server but it won't hurt to ignore them anyway while at the same time taking care of favicons -- THE serious offenders.
-      return @app.call(env) if env["PATH_INFO"] =~ /\.jpg|jpeg|png|ico|gif|pdf|tif|tiff|doc|xls|docx|xlsx|css|js$/i
-        
       ::Motivoo::Context.create(env) do |tracker, request|
-        ::Motivoo::Visit.track(tracker, request) do |tracker, request|
-          event, headers, body = @app.call(request.env)
-
-          response = Response.new(body, event, headers)
-          response  # tracker created user data and calls serialize_into(response) and response.finish
+        if track_path?(request)
+          ::Motivoo::Visit.track(tracker, request) do |tracker, request|
+            call_wrapped_app(request) 
+          end
+        else
+          call_wrapped_app(request)
         end
       end
-   end
+    end
+    
+    protected
+    
+    def track_path?(request)
+      # FIX: No cookies passed by Chrome when it requests favicon.ico/png which results in new user data record being 
+      # created for every request. AFAIK, IE keeps a separate cookie store for favicon requests. 
+      # Actually, we don't care about binary files at all, so while we're at it, let's ignore them all. In production environment, 
+      # most of these assets will be fetched from an asset server but it won't hurt to ignore them anyway while at the same time 
+      # taking care of favicons -- THE serious offenders.
+      is_binary = request.env["PATH_INFO"] =~ /\.jpg|jpeg|png|ico|gif|pdf|tif|tiff|doc|xls|docx|xlsx|css|js$/i
+      !is_binary
+    end
+    
+    def call_wrapped_app(request)
+      event, headers, body = @app.call(request.env)
+      Response.new(body, event, headers)
+    end
   end
 end
