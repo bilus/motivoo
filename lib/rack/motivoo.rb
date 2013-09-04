024f244c37da43deb66c968835dda3d8ce13a481
Martin Bilski <martinb@criticue.com>
2013-09-04 15:33:48 +0200

    Refactored anti-bot protection.

--- lib/rack/motivoo.rb
+++ lib/rack/motivoo.rb
@@ -1,6 +1,6 @@
 require_relative '../motivoo/context'
 require_relative '../motivoo/visit'
-require 'erb'
+require_relative './bot_protection'
 
 module Rack
   
@@ -12,13 +12,11 @@ module Rack
   class Motivoo
     def initialize(app)
       @app = app
-      if ::Motivoo.configuration.bot_protect_js
-        @app = bot_protected(app)
-      end
+      @app = BotProtection.maybe_protect(app)
     end
     
     def call(env)
-      with_context_for(env) do |tracker, request|
+      @app.with_context_for(env) do |tracker, request|
         if track_path?(request)
           ::Motivoo::Visit.track(tracker, request) do |tracker, request|
             app_response_to(request) 
@@ -30,16 +28,6 @@ module Rack
     end
     
     protected
-    
-    def with_context_for(env, &block)
-      m = force_create_context?(env) ? :create! : :create
-      ::Motivoo::Context.send(m, env, &block)
-    end
-    
-    def force_create_context?(env)
-      config = ::Motivoo.configuration
-      !config.bot_protect_js || env["PATH_INFO"] == config.bot_protect_path
-    end
     
     def track_path?(request)
       # FIX: No cookies passed by Chrome when it requests favicon.ico/png which results in new user data record being 
@@ -54,58 +42,6 @@ module Rack
     def app_response_to(request)
       status, headers, body = @app.call(request.env)
       Response.new(body, status, headers)
-    end
-    
-    ####
-    
-    def read_bot_protect_js
-      template = ::File.read(::File.expand_path("../../assets/javascripts/motivoo.js.erb", __FILE__))
-      ERB.new(template).result
-    end
-    
-    def bot_protected(app)
-      config = ::Motivoo.configuration
-      lambda do |env|
-        case env["PATH_INFO"]
-        when config.bot_protect_js_path
-          generate_response(200, "text/javascript", read_bot_protect_js)
-        when config.bot_protect_path
-          if env["REQUEST_METHOD"] == "POST"
-            generate_response(200, "application/json", '{"status":"ok"}')
-          else
-            generate_response(404, "text/html", "<h1>Not found</h1>")
-          end
-        else
-          status, headers, body = app.call(env)
-          inject_bot_protect_js(status, headers, body)
-        end
-      end
-    end
-    
-    def bot_protect_script_element
-      "
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = '#{::Motivoo.configuration.bot_protect_js_path}';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'motivoo'));</script>"
-    end
-        
-    def inject_bot_protect_js(status, headers, body)
-      se = bot_protect_script_element
-      body.each {|b| b.gsub!(/(<\/head>|<\/body>|<\/html\/>)/i, "#{se}" + '\1')}
-      headers["Content-Length"] = (headers["Content-Length"].to_i + se.length).to_s
-      [status, headers, body]
-    end
-    
-    def generate_response(status, content_type, body)
-      [
-        status, 
-        {"Content-Type" => content_type, "Content-Length" => body.length.to_s},
-        [body]
-      ]
     end
   end
 end
--- motivoo.gemspec
+++ motivoo.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.platform = Gem::Platform::RUBY
   s.name = 'motivoo'
-  s.version = '0.8.3'
+  s.version = '0.9'
   s.summary = 'Motivoo - AARRR Rack middleware.'
   s.description = ''
 
