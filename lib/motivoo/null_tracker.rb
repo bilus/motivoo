4afb946f7abd7465decc808d74c1e40a4eef9b28
Martin Bilski <martinb@criticue.com>
2013-09-04 15:45:06 +0200

    FIX: NullTracker does not correctly serialize/deserialize itself from env hash.

--- lib/motivoo/null_tracker.rb
+++ lib/motivoo/null_tracker.rb
@@ -3,5 +3,13 @@ require 'nullobject'
 module Motivoo
   class NullTracker
     include Null
+
+    def serialize_into(env)
+      env.merge("null_tracker" => self)
+    end
+    
+    def self.deserialize_from(env)
+      env["null_tracker"] or raise "NullTracker couldn't be found in the env hash. Internal error."
+    end
   end
 end
